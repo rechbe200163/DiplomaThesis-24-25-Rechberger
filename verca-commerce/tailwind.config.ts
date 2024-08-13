4808f60c169b5079f8f9bfe03a07717f8b35e2a6
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-08-13 18:59:35 +0200

    now also added support for shadcn

--- verca-commerce/tailwind.config.ts
+++ verca-commerce/tailwind.config.ts
@@ -74,7 +74,7 @@ const config = {
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), require("tailwindcss-animate")],
 } satisfies Config;
 
 export default config;
