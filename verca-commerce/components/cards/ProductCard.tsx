a3f28426269a3711291a5ab69de25dcce5311ed8
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-08-14 14:39:09 +0200

    updated visuales

--- verca-commerce/components/cards/ProductCard.tsx
+++ verca-commerce/components/cards/ProductCard.tsx
@@ -2,6 +2,7 @@ import { Product } from "@prisma/client";
 import React from "react";
 import Image from "next/image";
 import { ProductWithCategoryNames } from "@/lib/types";
+import Link from "next/link";
 
 const ProductCard = ({ product }: { product: ProductWithCategoryNames }) => {
   const isAddedLast7Days =
@@ -9,7 +10,10 @@ const ProductCard = ({ product }: { product: ProductWithCategoryNames }) => {
     new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
 
   return (
-    <div className="card bg-base-100 w-96 shadow-xl ">
+    <Link
+      href={`/shop/product/${product.id}`}
+      className="card bg-base-100 w-96 shadow-xl "
+    >
       <figure>
         <Image
           src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
@@ -23,18 +27,29 @@ const ProductCard = ({ product }: { product: ProductWithCategoryNames }) => {
           {product.name}
           {isAddedLast7Days && <div className="badge badge-secondary">NEW</div>}
         </h2>
-        <p>{product.description}</p>
-        <div className="card-actions justify-end">
-          <div className="card-actions justify-end">
+        <p>
+          {product.description.length >= 30
+            ? product.description.slice(0, 30) + "..."
+            : product.description}
+        </p>
+        <div className="card-actions justify-between items-center">
+          <span className="card-actions ">
+            {product.stock <= 5 ? (
+              <div className="badge badge-error font-bold antialiased">
+                only {product.stock} left
+              </div>
+            ) : null}
+          </span>
+          <span>
             {product.categories.map((category) => (
               <div key={category.category.name} className="badge badge-outline">
                 {category.category.name}
               </div>
             ))}
-          </div>
+          </span>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
