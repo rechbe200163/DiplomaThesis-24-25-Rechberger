75b906dd909cb13cacb16cd098f9fac9409a6099
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-08-14 11:13:19 +0200

    added role check for user

--- verca-commerce/lib/utils.ts
+++ verca-commerce/lib/utils.ts
@@ -1,6 +1,19 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { auth } from "@/auth";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export async function checkUserRole() {
+  const session = await auth();
+
+  console.log(session);
+
+  if (!session) {
+    return null;
+  }
+
+  return session.user.role;
 }
