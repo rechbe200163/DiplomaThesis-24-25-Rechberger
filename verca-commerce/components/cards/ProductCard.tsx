0a5b7a4d8034acfe3f48377480b3284143f17af8
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-08-14 11:04:20 +0200

    updated component to use next/image component fo lazy loading and checking if product is new or not

--- verca-commerce/components/cards/ProductCard.tsx
+++ verca-commerce/components/cards/ProductCard.tsx
@@ -1,23 +1,37 @@
+import { Product } from "@prisma/client";
 import React from "react";
+import Image from "next/image";
+import { ProductWithCategoryNames } from "@/lib/types";
 
-const ProductCard = () => {
+const ProductCard = ({ product }: { product: ProductWithCategoryNames }) => {
+  const isAddedLast7Days =
+    new Date(product.createdAt) >=
+    new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
+
   return (
-    <div className="card bg-base-100 w-96 shadow-xl">
+    <div className="card bg-base-100 w-96 shadow-xl ">
       <figure>
-        <img
+        <Image
           src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
           alt="Shoes"
+          width={400}
+          height={400}
         />
       </figure>
-      <div className="card-body">
-        <h2 className="card-title">
-          Shoes!
-          <div className="badge badge-secondary">NEW</div>
+      <div className="card-body rounded-xl text-gray-400">
+        <h2 className="card-title ">
+          {product.name}
+          {isAddedLast7Days && <div className="badge badge-secondary">NEW</div>}
         </h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <p>{product.description}</p>
         <div className="card-actions justify-end">
-          <div className="badge badge-outline">Fashion</div>
-          <div className="badge badge-outline">Products</div>
+          <div className="card-actions justify-end">
+            {product.categories.map((category) => (
+              <div key={category.category.name} className="badge badge-outline">
+                {category.category.name}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
