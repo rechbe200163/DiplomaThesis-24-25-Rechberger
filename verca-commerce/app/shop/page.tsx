cba4dc49189a1383b622dc0e939a4f1998212b18
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-08-14 14:39:26 +0200

    added grid layout for responsivness

--- verca-commerce/app/shop/page.tsx
+++ verca-commerce/app/shop/page.tsx
@@ -7,7 +7,7 @@ const dynamic = "force-dynamic";
 async function ShopPage() {
   const products = await getAllProducts();
   return (
-    <div className="flex p-10 space-x-10">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 items-center  gap-4 p-4">
       {products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
