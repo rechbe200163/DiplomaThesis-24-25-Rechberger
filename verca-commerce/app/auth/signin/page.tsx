49a74db2e53461c35a1e3030e50c0dfbf6595ca4
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-08-16 09:11:25 +0200

    adde custom singin and signup pages

---
+++ verca-commerce/app/auth/signin/page.tsx
@@ -0,0 +1,12 @@
+import AuthForm from "@/components/AuthForm";
+import React from "react";
+
+const SignIn = () => {
+  return (
+    <div>
+      <AuthForm type="sign-in" />
+    </div>
+  );
+};
+
+export default SignIn;
