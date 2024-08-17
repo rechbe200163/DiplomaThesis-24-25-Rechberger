cd03992deed6b670583523e7fc52c875ebafacb1
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-08-17 14:14:29 +0200

    added new compoentn for CartPage

---
+++ verca-commerce/components/cards/CartSummaryCard.tsx
@@ -0,0 +1,54 @@
+import { getCartByUserId } from "@/lib/data";
+import { formatPrice } from "@/lib/utils";
+import React from "react";
+
+async function CartSummaryCard({ customerId }: { customerId: string }) {
+  const prices = await getCartByUserId(customerId);
+
+  const subtotal = prices.products.reduce(
+    (acc, product) => acc + product.product.price,
+    0
+  );
+  const tax = 800;
+  const shipping = 500;
+
+  return (
+    <div className=" w-full md:w-1/3">
+      <div className="bg-white shadow-xl rounded-2xl p-6 sticky top-20">
+        <h2 className="text-2xl font-semibold text-gray-800 mb-4">Summary</h2>
+        {prices.products.map((product) => (
+          <div
+            key={product.product.name}
+            className="flex justify-between items-center mb-2"
+          >
+            <div className="text-lg font-medium text-gray-700">
+              {product.product.name}
+            </div>
+            <div className="text-lg font-medium text-gray-700">
+              {formatPrice(product.product.price)}
+            </div>
+          </div>
+        ))}
+        <div className="border-t border-gray-200 my-4"></div>
+
+        <div className="text-lg font-medium text-gray-700 mb-2">
+          Subtotal: {formatPrice(subtotal)}
+        </div>
+        <div className="text-lg font-medium text-gray-700 mb-2">
+          Shipping: {formatPrice(shipping)}
+        </div>
+        <div className="text-lg font-medium text-gray-700 mb-2">
+          Tax: {formatPrice(tax)}
+        </div>
+        <div className="text-xl font-bold text-gray-800 mt-4 border-t pt-4">
+          Total: {formatPrice(subtotal + tax + shipping)}
+        </div>
+        <button className="w-full bg-green-500 text-white py-2 mt-6 rounded-lg hover:bg-green-600 transition">
+          Proceed to Checkout
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CartSummaryCard;
---
+++ verca-commerce/components/cards/ProcutsCartCard.tsx
@@ -0,0 +1,55 @@
+import { getCartByUserId } from "@/lib/data";
+import Image from "next/image";
+import React from "react";
+
+async function ProcutsCartCard({ customerId }: { customerId: string }) {
+  const products = await getCartByUserId(customerId);
+
+  console.log(products);
+
+  return (
+    <div className="flex flex-col gap-6 p-4 md:p-0 w-full md:w-2/3 overflow-auto">
+      {products.products.map((product) => (
+        <div className="card card-side shadow-xl rounded-lg overflow-hidden bg-white">
+          <figure className="w-1/3">
+            <Image
+              src={
+                product.product.imagePath ||
+                "https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+              }
+              alt="Movie"
+              className="object-cover w-full h-full"
+              width={400}
+              height={400}
+            />
+          </figure>
+          <div className="card-body flex flex-col gap-4 p-4 w-2/3">
+            <div className="text-lg font-semibold text-gray-800">
+              {product.product.name}
+            </div>
+            <div className="badge badge-outline text-gray-600">
+              {product.product.stock}
+            </div>
+            <div className="text-gray-600">Quantity: 1</div>
+            <div className="flex items-center justify-between mt-auto">
+              <select className="badge badge-ghost badge-lg" defaultValue={1}>
+                {Array(11)
+                  .fill(null)
+                  .map((_, index) => (
+                    <option key={index} value={index}>
+                      {index}
+                    </option>
+                  ))}
+              </select>
+              <button className="badge badge-ghost badge-lg border-error text-error hover:bg-error hover:text-white transition">
+                Remove
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ProcutsCartCard;
