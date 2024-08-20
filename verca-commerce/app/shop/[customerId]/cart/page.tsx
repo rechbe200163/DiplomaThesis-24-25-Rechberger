049283edb6b7da02c0a12435d56663cda923318c
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-08-20 17:23:05 +0200

    updated cart componetn

--- verca-commerce/app/shop/[customerId]/cart/page.tsx
+++ verca-commerce/app/shop/[customerId]/cart/page.tsx
@@ -4,9 +4,7 @@ import { getCartByUserId } from "@/lib/data";
 import { get } from "http";
 import React from "react";
 
-async function CartPage({ params }: { params: { customerId: string } }) {
-  const cart = await getCartByUserId(params.customerId);
-
+function CartPage({ params }: { params: { customerId: string } }) {
   return (
     <div className="p-5 md:p-10 space-y-10 bg-gray-50 min-h-screen">
       <div className="text-center md:text-left">
@@ -14,8 +12,9 @@ async function CartPage({ params }: { params: { customerId: string } }) {
           Shopping Cart
         </span>
         <div className="flex justify-center md:justify-start space-x-2 mt-2">
-          <p className="text-lg font-bold text-gray-700">4 items</p>
-          <h1 className="text-lg text-gray-600">in your cart.</h1>
+          <h1 className="text-lg font-semibold text-gray-600">
+            proceed with the checkout
+          </h1>
         </div>
       </div>
       <div className="flex flex-col md:flex-row justify-between gap-10">
