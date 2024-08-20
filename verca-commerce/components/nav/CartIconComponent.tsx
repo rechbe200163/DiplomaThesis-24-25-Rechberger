a09962b6b12e49e4a4dfec42bedce43839c13346
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-08-20 16:58:25 +0200

    moved cart icon to seperate file for more seperation

---
+++ verca-commerce/components/nav/CartIconComponent.tsx
@@ -0,0 +1,29 @@
+import { auth } from "@/auth";
+import { fetchProductsInCart } from "@/lib/data";
+import prisma from "@/prisma/client";
+import Link from "next/link";
+import React from "react";
+import { IoCartOutline } from "react-icons/io5";
+
+async function CartIconComponent() {
+  const session = await auth();
+
+  const productsCount = await fetchProductsInCart(session?.user.id || "");
+
+  return (
+    <div>
+      <Link href={`/shop/${session?.user.id}/cart`}>
+        <div className="flex relative">
+          <IoCartOutline size={35} />
+          {productsCount?._count.products > 0 && (
+            <div className="badge badge-primary badge-md absolute -top-2  -right-4">
+              {productsCount?._count.products}
+            </div>
+          )}
+        </div>
+      </Link>
+    </div>
+  );
+}
+
+export default CartIconComponent;
