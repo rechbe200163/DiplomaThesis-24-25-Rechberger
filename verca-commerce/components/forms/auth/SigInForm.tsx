0011dc00e75ae97a0ce2be258419a0b4c9960d08
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-08-22 22:17:15 +0200

    component now shows error message after wrong credentials

--- verca-commerce/components/forms/auth/SigInForm.tsx
+++ verca-commerce/components/forms/auth/SigInForm.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import SignIn from "@/app/auth/signup/page";
-import { Alert } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -12,10 +10,13 @@ import { useState } from "react";
 const SignInForm = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const callbackUrl = searchParams.get("callbackUrl") || "/shop";
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const error = searchParams.get("error");
+  const errorMessage =
+    error === "CredentialsSignin"
+      ? "Invalid email or password"
+      : "Something Went Wrong, please try again";
   const [isLoading, setLoading] = useState(false);
 
   const onSubmit = async (e: React.FormEvent) => {
@@ -23,17 +24,11 @@ const SignInForm = () => {
     setLoading(true);
     try {
       const res = await signIn("credentials", {
-        redirect: false,
+        redirect: true,
         email,
         password,
-        callbackUrl,
       });
       console.log("Res", res);
-      if (!res?.error) {
-        router.push(callbackUrl);
-      } else {
-        setError("Invalid email or password");
-      }
     } catch (err: any) {
     } finally {
       setLoading(false);
@@ -41,36 +36,57 @@ const SignInForm = () => {
   };
 
   return (
-    <form onSubmit={onSubmit} className="space-y-12 w-full sm:w-[400px]">
-      {error && <div className="alert alert-error">{error}</div>}
-      <div className="grid w-full items-center gap-1.5">
-        <Label htmlFor="email">Email</Label>
-        <Input
-          className="w-full"
-          required
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          id="email"
-          type="email"
-        />
-      </div>
-      <div className="grid w-full items-center gap-1.5">
-        <Label htmlFor="password">Password</Label>
-        <Input
-          className="w-full"
-          required
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          id="password"
-          type="password"
-        />
-      </div>
-      <div className="w-full">
-        <Button className="w-full" size="lg">
-          {isLoading ? "Loading..." : "Sign In"}
-        </Button>
-      </div>
-    </form>
+    <>
+      {error && (
+        <div role="alert" className="alert alert-error">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 shrink-0 stroke-current"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <span>
+            <strong>Error:</strong> {error}
+          </span>
+        </div>
+      )}
+      <form onSubmit={onSubmit} className="space-y-12 w-full sm:w-[400px]">
+        <div className="grid w-full items-center gap-1.5">
+          <Label htmlFor="email">Email</Label>
+          <Input
+            className="w-full"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            id="email"
+            type="email"
+          />
+        </div>
+        <div className="grid w-full items-center gap-1.5">
+          <Label htmlFor="password">Password</Label>
+          <Input
+            className="w-full"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            id="password"
+            type="password"
+          />
+        </div>
+        <div className="w-full">
+          <Button className="w-full" size="lg">
+            {isLoading ? "Loading..." : "Sign In"}
+          </Button>
+        </div>
+      </form>
+    </>
   );
 };
 
