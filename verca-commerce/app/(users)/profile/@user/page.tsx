492b4dfeb319ed1f9858e6bde6f6af667c908169
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-08-22 22:02:57 +0200

    added a user component

--- verca-commerce/app/(users)/profile/@user/page.tsx
+++ verca-commerce/app/(users)/profile/@user/page.tsx
@@ -1,8 +1,14 @@
 import SignOutPage from "@/app/auth/signout/page";
+import UserDetailsCard from "@/components/cards/profile/UserDetailsCard";
 import React from "react";
 
 const UserProfile = () => {
-  return <div>UserProfile</div>;
+  return (
+    <div>
+      <SignOutPage />
+      <UserDetailsCard />
+    </div>
+  );
 };
 
 export default UserProfile;
