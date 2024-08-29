cf385bff5386740a742bba70ad06e62edfef3d06
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-08-29 10:34:09 +0200

    Added InteractiviyComponent to dashboard navigation

---
+++ verca-commerce/components/nav/dashboard/InteractiviyComponent.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BiBell } from "react-icons/bi";
+import { PiQuestionMarkBold, PiQuestionMarkFill } from "react-icons/pi";
+
+function InteractiviyComponent() {
+  return (
+    <section className="flex flex-row space-x-4">
+      <div className="flex items-center space-x-4 text-black-1">
+        <PiQuestionMarkBold className="w-6 h-6" />
+      </div>
+      <div className="flex items-center space-x-4 text-black-1">
+        <BiBell className="w-6 h-6" />
+      </div>
+    </section>
+  );
+}
+
+export default InteractiviyComponent;
