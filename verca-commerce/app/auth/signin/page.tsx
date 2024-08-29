d9f7ecda7cc27c0a8e22a3ff71c6c1e2b32d5e2a
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-08-29 10:13:21 +0200

    Added lazy loading for SignInForm component

--- verca-commerce/app/auth/signin/page.tsx
+++ verca-commerce/app/auth/signin/page.tsx
@@ -1,17 +1,20 @@
 import SignInForm from "@/components/forms/auth/SigInForm";
 import Link from "next/link";
+import { Suspense } from "react";
 
 export default function SignIn() {
   return (
     <div className="h-screen w-screen flex justify-center items-center bg-slate-100">
       <div className="sm:shadow-xl px-8 pb-8 pt-12 sm:bg-white rounded-xl space-y-12">
         <h1 className="font-semibold text-2xl">Login</h1>
-        <SignInForm />
+        <Suspense fallback={<div>Loading...</div>}>
+          <SignInForm />
+        </Suspense>
         <p className="text-center">
-          Need to create an account?{" "}
+          Need to create an account?
           <Link className="text-indigo-500 hover:underline" href="/register">
             Create Account
-          </Link>{" "}
+          </Link>
         </p>
       </div>
     </div>
