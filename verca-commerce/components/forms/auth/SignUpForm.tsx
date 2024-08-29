a3b14551aeb2aaeb9da7ba0773f4232e7b19ebfb
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-08-29 10:15:25 +0200

    Removed unused code and toast message from SignUpForm

--- verca-commerce/components/forms/auth/SignUpForm.tsx
+++ verca-commerce/components/forms/auth/SignUpForm.tsx
@@ -15,7 +15,6 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { toast } from "@/components/ui/use-toast";
 import {
   Select,
   SelectContent,
@@ -74,14 +73,6 @@ export function SignUpForm() {
         data.streetNumber
       );
     } catch (error) {}
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-    });
   }
 
   return (
