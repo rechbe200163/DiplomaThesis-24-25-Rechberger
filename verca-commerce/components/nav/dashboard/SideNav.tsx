c112cb3d7af7d61369550b91f941d5cb02a5f1b4
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-08-29 19:05:04 +0200

    Updated SideNav component layout and dependencies

--- verca-commerce/components/nav/dashboard/SideNav.tsx
+++ verca-commerce/components/nav/dashboard/SideNav.tsx
@@ -1,24 +1,15 @@
-import Link from "next/link";
 import NavLinksProfile from "./NavLinksProfile";
-import Image from "next/image";
-import ProfileIconComponent from "./ProfileIconComponent";
 import CompanyIconComponent from "./CompanyIconComponent";
-import { Suspense } from "react";
 import Search from "@/components/search/SearchComponent";
 
 export default function SideNav() {
   return (
     <section className="flex h-full flex-col px-3 py-4 md:px-2">
-      <Suspense fallback={<div>Loading...</div>}>
-        <CompanyIconComponent />
-      </Suspense>
+      <CompanyIconComponent />
+      <div className="pb-3">
+        <Search placeholder="Search anything" />
+      </div>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-        <div className="flex-1 max-w-screen-xl">
-          <Suspense fallback={<div>Loading Search...</div>}>
-            <Search placeholder="Search anything" />
-          </Suspense>
-        </div>
-
         <NavLinksProfile />
       </div>
     </section>
