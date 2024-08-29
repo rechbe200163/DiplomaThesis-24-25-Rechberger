4817b9316c897696fa520bbd59641eb9b5afb46d
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-08-29 10:14:14 +0200

    added custom styles

--- verca-commerce/tailwind.config.ts
+++ verca-commerce/tailwind.config.ts
@@ -52,6 +52,42 @@ const config = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        success: {
+          25: "#F6FEF9",
+          50: "#ECFDF3",
+          100: "#D1FADF",
+          600: "#039855",
+          700: "#027A48",
+          900: "#054F31",
+        },
+        blue: {
+          25: "#F5FAFF",
+          100: "#D1E9FF",
+          500: "#2E90FA",
+          600: "#1570EF",
+          700: "#175CD3",
+          900: "#194185",
+        },
+        sky: {
+          1: "#F3F9FF",
+        },
+        black: {
+          1: "#00214F",
+          2: "#344054",
+        },
+        gray: {
+          25: "#FCFCFD",
+          200: "#EAECF0",
+          300: "#D0D5DD",
+          500: "#667085",
+          600: "#475467",
+          700: "#344054",
+          900: "#101828",
+        },
+      },
+      fontFamily: {
+        inter: "var(--font-inter)",
+        "ibm-plex-serif": "var(--font-ibm-plex-serif)",
       },
       borderRadius: {
         lg: "var(--radius)",
