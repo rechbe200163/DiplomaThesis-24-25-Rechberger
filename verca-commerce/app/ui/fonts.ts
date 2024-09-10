e304ff5b759e0c7a30508bbc5de79a4e95dc6aaf
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-09-10 14:40:16 +0200

    Add font files and styles for Inter, Lusitana, Raleway, Roboto, and Karla

---
+++ verca-commerce/app/ui/fonts.ts
@@ -0,0 +1,23 @@
+import { Inter, Lusitana, Raleway, Roboto, Karla } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+export const raleway = Raleway({
+  weight: ["500", "700"],
+  subsets: ["latin"],
+});
+
+export const roboto = Roboto({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+});
+
+export const karla = Karla({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
