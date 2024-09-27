d8c0d300b4108d1d9d6fc8147407481a8dcd0004
Michael Rechberger <rechbe200163@sr.htlweiz.at>
2024-09-27 11:05:31 +0200

    refactored

--- verca-commerce/app/shop/product/[productId]/page.tsx
+++ verca-commerce/app/shop/product/[productId]/page.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import Image from "next/image";
-import { getProductById } from "@/lib/data";
-import { formatPrice } from "@/lib/utils";
+import React from 'react';
+import Image from 'next/image';
+import { getProductById } from '@/lib/data';
+import { formatPrice } from '@/lib/utils';
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@/components/ui/accordion";
-import { BiAddToQueue } from "react-icons/bi";
-import AddToCartForm from "@/components/forms/addToCard";
+} from '@/components/ui/accordion';
+import { BiAddToQueue } from 'react-icons/bi';
+import AddToCartForm from '@/components/forms/addToCard';
 
 interface ProductDetailsPageProps {
   params: {
@@ -21,25 +21,25 @@ async function ProductDetailsPage({ params }: ProductDetailsPageProps) {
   const product = await getProductById(params.productId);
 
   return (
-    <div className="p-4 md:p-20">
-      <div className="card flex flex-col xl:flex-row w-full shadow-xl">
-        <figure className="flex-none w-full xl:w-1/3">
+    <div className='p-4 md:p-20'>
+      <div className='card flex flex-col xl:flex-row w-full shadow-xl'>
+        <figure className='flex-none w-full xl:w-1/3'>
           <Image
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            src='https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp'
             alt={product.name}
             width={500}
             height={500}
-            className="rounded-xl xl:rounded-none object-cover w-full"
+            className='rounded-xl xl:rounded-none object-cover w-full'
           />
-          <figcaption className="sr-only">{product.name}</figcaption>
+          <figcaption className='sr-only'>{product.name}</figcaption>
         </figure>
-        <div className="card-body flex-1 p-4 md:p-6">
-          <h2 className="card-title text-2xl md:text-4xl">{product.name}</h2>
-          <p className="hidden md:block mt-4 text-sm md:text-base">
+        <div className='card-body flex-1 p-4 md:p-6'>
+          <h2 className='card-title text-2xl md:text-4xl'>{product.name}</h2>
+          <p className='hidden md:block mt-4 text-sm md:text-base'>
             {product.description.slice(0, 400)}
           </p>
-          <div className="card-actions justify-between mt-4 md:mt-8">
-            <span className="stat-value text-xl md:text-2xl">
+          <div className='card-actions justify-between mt-4 md:mt-8'>
+            <span className='stat-value text-xl md:text-2xl'>
               {formatPrice(product.price)}
             </span>
             <AddToCartForm productId={product.id} />
@@ -47,10 +47,10 @@ async function ProductDetailsPage({ params }: ProductDetailsPageProps) {
         </div>
       </div>
       {/* Mobile Accordion for Description */}
-      <div className="collapse mt-4 shadow-xl from from-gray-200 bg-gradient-to-t hover:bg-gray-300 transition-colors ease-in-out duration-500">
-        <input type="checkbox" />
-        <div className="collapse-title text-xl font-medium">Description</div>
-        <div className="collapse-content">
+      <div className='md:hidden collapse mt-4 shadow-xl from from-gray-200 bg-gradient-to-t hover:bg-gray-300 transition-colors ease-in-out duration-500'>
+        <input type='checkbox' />
+        <div className='collapse-title text-xl font-medium'>Description</div>
+        <div className='collapse-content'>
           <p>{product.description}</p>
         </div>
       </div>
