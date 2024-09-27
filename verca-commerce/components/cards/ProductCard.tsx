3d9c1310d3df12685ac7ebc991488977d04b1cfb
Michael Rechberger <rechbe200163@sr.htlweiz.at>
2024-09-27 11:05:06 +0200

    improved styling

--- verca-commerce/components/cards/ProductCard.tsx
+++ verca-commerce/components/cards/ProductCard.tsx
@@ -1,8 +1,8 @@
-import { Product } from "@prisma/client";
-import React from "react";
-import Image from "next/image";
-import { ProductWithCategoryNames } from "@/lib/types";
-import Link from "next/link";
+import { Product } from '@prisma/client';
+import React from 'react';
+import Image from 'next/image';
+import { ProductWithCategoryNames } from '@/lib/types';
+import Link from 'next/link';
 
 const ProductCard = ({ product }: { product: ProductWithCategoryNames }) => {
   const isAddedLast7Days =
@@ -12,41 +12,40 @@ const ProductCard = ({ product }: { product: ProductWithCategoryNames }) => {
   return (
     <Link
       href={`/shop/product/${product.id}`}
-      className="card bg-base-100 w-96 shadow-xl "
+      className='card bg-base-100 shadow-xl'
     >
       <figure>
         <Image
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Shoes"
+          src='https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp'
+          alt='Shoes'
           width={400}
           height={400}
+          className='w-full rounded-t-xl'
         />
       </figure>
-      <div className="card-body rounded-xl text-gray-400">
-        <h2 className="card-title ">
+      <div className='card-body rounded-xl text-gray-400 w-full'>
+        <h2 className='card-title '>
           {product.name}
-          {isAddedLast7Days && <div className="badge badge-secondary">NEW</div>}
+          {isAddedLast7Days && <div className='badge badge-secondary'>NEW</div>}
         </h2>
         <p>
           {product.description.length >= 30
-            ? product.description.slice(0, 30) + "..."
+            ? product.description.slice(0, 30) + '...'
             : product.description}
         </p>
-        <div className="card-actions justify-between items-center">
-          <span className="card-actions ">
-            {product.stock <= 5 ? (
-              <div className="badge badge-error font-bold antialiased">
-                only {product.stock} left
-              </div>
-            ) : null}
-          </span>
-          <span>
+        <div className='card-actions justify-between items-center'>
+          {product.stock <= 5 && (
+            <div className='badge badge-error font-bold'>
+              only {product.stock} left
+            </div>
+          )}
+          <div>
             {product.categories.map((category) => (
-              <div key={category.category.name} className="badge badge-outline">
+              <div key={category.category.name} className='badge badge-outline'>
                 {category.category.name}
               </div>
             ))}
-          </span>
+          </div>
         </div>
       </div>
     </Link>
