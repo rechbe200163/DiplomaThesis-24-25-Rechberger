3369ac6dd9eb17b58cac852f9ff8325c5e18a7d5
Michael Rechberger <rechbe200163@sr.htlweiz.at>
2024-09-27 11:04:58 +0200

    refactord

--- verca-commerce/app/shop/page.tsx
+++ verca-commerce/app/shop/page.tsx
@@ -1,13 +1,13 @@
-import ProductCard from "@/components/cards/ProductCard";
-import { getAllProducts } from "@/lib/data";
-import React from "react";
+import ProductCard from '@/components/cards/ProductCard';
+import { getAllProducts } from '@/lib/data';
+import React from 'react';
 
-const dynamic = "force-dynamic";
+const dynamic = 'force-dynamic';
 
 async function ShopPage() {
   const products = await getAllProducts();
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 items-center  gap-4 p-4">
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 items-center gap-4 p-4'>
       {products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
