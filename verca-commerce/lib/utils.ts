38416bd11c1f90763c7105a5af7c8cc8a90a03a9
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-10-01 17:05:29 +0200

    Refactor import statements in utils.ts

--- verca-commerce/lib/utils.ts
+++ verca-commerce/lib/utils.ts
@@ -1,18 +1,18 @@
-import { auth } from "@/auth";
-import { BusinessSector } from "@prisma/client";
-import { type ClassValue, clsx } from "clsx";
-import { count } from "console";
-import { twMerge } from "tailwind-merge";
-import { z } from "zod";
+import { auth } from '@/auth';
+import { BusinessSector } from '@prisma/client';
+import { type ClassValue, clsx } from 'clsx';
+import { count } from 'console';
+import { twMerge } from 'tailwind-merge';
+import { z } from 'zod';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 export function formatPrice(price: number) {
-  const formattedPrice = (price / 100).toLocaleString("de-at", {
-    style: "currency",
-    currency: "EUR",
+  const formattedPrice = (price / 100).toLocaleString('de-at', {
+    style: 'currency',
+    currency: 'EUR',
   });
 
   return formattedPrice;
@@ -33,12 +33,12 @@ export async function checkUserRole() {
 export const authSignUpFormSchema = () =>
   z
     .object({
-      firstName: z.string().min(1, "First name is required"),
-      lastName: z.string().min(1, "Last name is required"),
-      email: z.string().email("Invalid email address"),
-      phoneNumber: z.string().min(1, "Phone number is required"),
+      firstName: z.string().min(1, 'First name is required'),
+      lastName: z.string().min(1, 'Last name is required'),
+      email: z.string().email('Invalid email address'),
+      phoneNumber: z.string().min(1, 'Phone number is required'),
 
-      companyNumber: z.string().min(1, "Company number is required"),
+      companyNumber: z.string().min(1, 'Company number is required'),
       businessSector: z.enum([
         BusinessSector.AGRICULTURE,
         BusinessSector.CONSTRUCTION,
@@ -51,21 +51,21 @@ export const authSignUpFormSchema = () =>
         BusinessSector.TECHNOLOGY,
         BusinessSector.TRANSPORTATION,
       ]),
-      city: z.string().min(1, "City is required"),
-      country: z.string().min(1, "Country is required"),
-      postCode: z.string().min(1, "Postal code is required"),
-      state: z.string().min(1, "State is required"),
-      streetNumber: z.string().min(1, "Street number is required"),
-      streetName: z.string().min(1, "Street name is required"),
+      city: z.string().min(1, 'City is required'),
+      country: z.string().min(1, 'Country is required'),
+      postCode: z.string().min(1, 'Postal code is required'),
+      state: z.string().min(1, 'State is required'),
+      streetNumber: z.string().min(1, 'Street number is required'),
+      streetName: z.string().min(1, 'Street name is required'),
 
-      password: z.string().min(6, "Password must be at least 6 characters"),
+      password: z.string().min(6, 'Password must be at least 6 characters'),
       confirmPassword: z
         .string()
-        .min(6, "Confirm password must be at least 6 characters"),
+        .min(6, 'Confirm password must be at least 6 characters'),
     })
     .refine((data) => data.password === data.confirmPassword, {
-      message: "Passwords do not match",
-      path: ["confirmPassword"],
+      message: 'Passwords do not match',
+      path: ['confirmPassword'],
     });
 
 export const authSignInFormSchema = () =>
