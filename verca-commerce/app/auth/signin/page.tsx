51502907702843af862a382948ba89e47bc86be9
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-10-10 17:06:19 +0200

    upated link for signUp page

--- verca-commerce/app/auth/signin/page.tsx
+++ verca-commerce/app/auth/signin/page.tsx
@@ -1,18 +1,18 @@
-import SignInForm from "@/components/forms/auth/SigInForm";
-import Link from "next/link";
-import { Suspense } from "react";
+import SignInForm from '@/components/forms/auth/SigInForm';
+import Link from 'next/link';
+import { Suspense } from 'react';
 
 export default function SignIn() {
   return (
-    <div className="h-screen w-screen flex justify-center items-center bg-slate-100">
-      <div className="sm:shadow-xl px-8 pb-8 pt-12 sm:bg-white rounded-xl space-y-12">
-        <h1 className="font-semibold text-2xl">Login</h1>
+    <div className='h-screen w-screen flex justify-center items-center bg-slate-100'>
+      <div className='sm:shadow-xl px-8 pb-8 pt-12 sm:bg-white rounded-xl space-y-12'>
+        <h1 className='font-semibold text-2xl'>Login</h1>
         <Suspense fallback={<div>Loading...</div>}>
           <SignInForm />
         </Suspense>
-        <p className="text-center">
+        <p className='text-center'>
           Need to create an account?
-          <Link className="text-indigo-500 hover:underline" href="/register">
+          <Link className='text-indigo-500 hover:underline' href='/auth/signup'>
             Create Account
           </Link>
         </p>
