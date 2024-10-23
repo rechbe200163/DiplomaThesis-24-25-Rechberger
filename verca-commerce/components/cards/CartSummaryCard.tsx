cc0e32c11b0327bcf3a6057206ee8b687580c885
Michael Rechberger <rechbe200163@sr.htlweiz.at>
2024-10-23 12:54:52 +0200

    Refactor CartSummaryCard component to use Button component for checkout and update subtotal calculation

--- verca-commerce/components/cards/CartSummaryCard.tsx
+++ verca-commerce/components/cards/CartSummaryCard.tsx
@@ -1,16 +1,17 @@
 import { getCartByUserId } from '@/lib/data';
 import { formatPrice } from '@/lib/utils';
 import React from 'react';
-import CheckOutForm from '../payment/CheckOutForm';
 import Link from 'next/link';
+import { Button } from '../ui/button';
 
 async function CartSummaryCard({ customerId }: { customerId: string }) {
-  const prices = await getCartByUserId(customerId);
+  const request = await getCartByUserId(customerId);
 
-  const subtotal = prices.products.reduce(
+  const subtotal = request.products.reduce(
     (acc, product) => acc + product.product.price,
     0
   );
+
   const tax = 0;
   const shipping = 0;
 
@@ -18,7 +19,7 @@ async function CartSummaryCard({ customerId }: { customerId: string }) {
     <div className=' w-full md:w-1/3'>
       <div className='bg-white shadow-xl rounded-2xl p-6 sticky top-20'>
         <h2 className='text-2xl font-semibold text-gray-800 mb-4'>Summary</h2>
-        {prices.products.map((product) => (
+        {request.products.map((product) => (
           <div
             key={product.product.name}
             className='flex justify-between items-center mb-2'
@@ -42,7 +43,13 @@ async function CartSummaryCard({ customerId }: { customerId: string }) {
         <div className='text-xl font-bold text-gray-800 mt-4 border-t pt-4'>
           Total: {formatPrice(subtotal + tax + shipping)}
         </div>
-        <Link href={`/shop/purchase/${customerId}`}>Proceed to Checkout</Link>
+        <div className='pt-4'>
+          <Button disabled={subtotal === 0}>
+            <Link href={`/shop/purchase/${customerId}`}>
+              Proceed to Checkout
+            </Link>
+          </Button>
+        </div>
       </div>
     </div>
   );
