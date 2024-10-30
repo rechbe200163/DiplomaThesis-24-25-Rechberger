2042e0e2ab0f8f4dbacfdbe2535ab27b16bfbeb4
Michael Rechberger <rechbe200163@sr.htlweiz.at>
2024-10-30 17:17:16 +0100

    Add DecreaseProductQuantity and IncreaseProductQuantity components for cart item quantity adjustments

---
+++ verca-commerce/components/forms/cart/decreaseProductQuantity.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import { updateQuantity } from '@/lib/actions/product.actions';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectValue,
+} from '../../ui/select';
+import { SelectTrigger } from '@radix-ui/react-select';
+import { useActionState } from 'react';
+import { Button } from '../../ui/button';
+import { MinusIcon, PlusIcon } from 'lucide-react';
+
+function DecreaseProductQuantity({
+  productId,
+  quantity,
+}: {
+  productId: string;
+  quantity: number;
+}) {
+  console.log('ProductId from UpdateProductQuantity', productId);
+
+  // Bind updateQuantity to productId
+  const decreaseProductQuantity = updateQuantity.bind(null, productId);
+  const [formState, action, isPending] = useActionState(
+    decreaseProductQuantity,
+    {
+      success: false,
+      errors: {
+        title: [''],
+      },
+    }
+  );
+
+  return (
+    <form action={action} className='flex items-center'>
+      <div className='justify-items-stretch'>
+        <Button
+          variant={'outline'}
+          size={'icon'}
+          type='submit'
+          disabled={quantity <= 1}
+        >
+          <MinusIcon />
+        </Button>
+        <input type='hidden' name='update' value={-1}></input>
+      </div>
+
+      {/* Display errors if any */}
+      {formState.errors?.title[0] && (
+        <div className='text-red-500 text-sm mt-2'>
+          {formState.errors.title.join(', ')}
+        </div>
+      )}
+    </form>
+  );
+}
+
+export default DecreaseProductQuantity;
---
+++ verca-commerce/components/forms/cart/increaseProductQuantity.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import { updateQuantity } from '@/lib/actions/product.actions';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectValue,
+} from '../../ui/select';
+import { SelectTrigger } from '@radix-ui/react-select';
+import { useActionState } from 'react';
+import { Button } from '../../ui/button';
+import { MinusIcon, PlusIcon } from 'lucide-react';
+
+function IncreaseProductQuantity({
+  productId,
+  quantity,
+}: {
+  productId: string;
+  quantity: number;
+}) {
+  // Bind updateQuantity to productId
+  const increaseProductQuantity = updateQuantity.bind(null, productId);
+  const [formState, action, isPending] = useActionState(
+    increaseProductQuantity,
+    {
+      success: false,
+      errors: {
+        title: [''],
+      },
+    }
+  );
+
+  return (
+    <form action={action} className='flex items-center'>
+      <div className='justify-items-stretch'>
+        <Button variant={'outline'} size={'icon'} type='submit'>
+          <PlusIcon />
+        </Button>
+        <input type='hidden' name='update' value={1}></input>
+      </div>
+
+      {/* Display errors if any */}
+      {formState.errors?.title[0] && (
+        <div className='text-red-500 text-sm mt-2'>
+          {formState.errors.title.join(', ')}
+        </div>
+      )}
+    </form>
+  );
+}
+
+export default IncreaseProductQuantity;
