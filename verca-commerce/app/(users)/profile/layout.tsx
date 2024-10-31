9680061231ba0a2283ba8acfcc175d7ac9c7d880
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-10-31 14:32:15 +0100

    Refactor layout and sign-in form components for improved code consistency and readability

--- verca-commerce/app/(users)/profile/layout.tsx
+++ verca-commerce/app/(users)/profile/layout.tsx
@@ -1,12 +1,12 @@
-import { auth } from "@/auth";
-import HeaderBox from "@/components/header/HeaderBox";
-import InteractiviyComponent from "@/components/nav/dashboard/InteractiviyComponent";
-import DashboardFooter from "@/components/nav/dashboard/ProfileIconComponent";
-import SideNav from "@/components/nav/dashboard/SideNav";
-import DashbaordFooterSkeleton from "@/components/skeletons/DashboardFooterSkeleton";
-import SideNavSkeleton from "@/components/skeletons/SideNavSkeleton";
-import Link from "next/link";
-import { Suspense } from "react";
+import { auth } from '@/auth';
+import HeaderBox from '@/components/header/HeaderBox';
+import InteractiviyComponent from '@/components/nav/dashboard/InteractiviyComponent';
+import DashboardFooter from '@/components/nav/dashboard/ProfileIconComponent';
+import SideNav from '@/components/nav/dashboard/SideNav';
+import DashbaordFooterSkeleton from '@/components/skeletons/DashboardFooterSkeleton';
+import SideNavSkeleton from '@/components/skeletons/SideNavSkeleton';
+import Link from 'next/link';
+import { Suspense } from 'react';
 
 export default async function Layout({
   user,
@@ -16,11 +16,10 @@ export default async function Layout({
   admin: React.ReactNode;
 }) {
   const session = await auth();
-  console.log("role", session?.user.role);
   if (!session?.user.role) {
     return (
-      <div className="flex h-screen items-center justify-center">
-        <Link href={"/auth/signin"} className="btn btn-primary">
+      <div className='flex h-screen items-center justify-center'>
+        <Link href={'/auth/signin'} className='btn btn-primary'>
           Login
         </Link>
       </div>
@@ -28,15 +27,15 @@ export default async function Layout({
   }
 
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden bg-gray-100">
+    <div className='flex h-screen flex-col md:flex-row md:overflow-hidden bg-gray-100'>
       {/* Sidebar */}
-      <aside className="flex-none w-full md:w-64 border-r-2 bg-white shadow-md flex flex-col justify-between">
+      <aside className='flex-none w-full md:w-64 border-r-2 bg-white shadow-md flex flex-col justify-between'>
         <div>
           <Suspense fallback={<SideNavSkeleton />}>
             <SideNav />
           </Suspense>
         </div>
-        <div className="p-4">
+        <div className='p-4'>
           <Suspense fallback={<DashbaordFooterSkeleton />}>
             <DashboardFooter />
           </Suspense>
@@ -44,21 +43,21 @@ export default async function Layout({
       </aside>
 
       {/* Main content */}
-      <div className="flex-grow flex flex-col">
+      <div className='flex-grow flex flex-col'>
         {/* Header with border */}
-        <div className="p-4 border-b-2 bg-white shadow-sm flex flex-row items-center gap-4 justify-between">
+        <div className='p-4 border-b-2 bg-white shadow-sm flex flex-row items-center gap-4 justify-between'>
           <HeaderBox
-            title="Welcome,"
-            subtext="welcome to your dashboard"
+            title='Welcome,'
+            subtext='welcome to your dashboard'
             name={session.user.lastName}
-            type="greeting"
+            type='greeting'
           />
           <InteractiviyComponent />
         </div>
 
         {/* Conditional content based on role */}
-        <div className="flex-grow p-4 overflow-auto">
-          {session.user.role === "USER" ? (
+        <div className='flex-grow p-4 overflow-auto'>
+          {session.user.role === 'USER' ? (
             <Suspense fallback={<div>Loading...</div>}>{user}</Suspense>
           ) : (
             <Suspense fallback={<div>Loading...</div>}>{admin}</Suspense>
