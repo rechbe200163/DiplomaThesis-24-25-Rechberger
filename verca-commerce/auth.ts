4ca7449a8e721f6b63b345430e875c03fd4d547d
Michael Rechberger <rechbe200163@sr.htlweiz.at>
2024-11-12 19:00:25 +0100

    updated auth logic now using customer reference number

--- verca-commerce/auth.ts
+++ verca-commerce/auth.ts
@@ -1,40 +1,42 @@
-import NextAuth, { DefaultSession } from "next-auth";
-import Credentials from "next-auth/providers/credentials";
-import prisma from "./prisma/client";
-import { compare } from "bcryptjs";
-import { Customer } from "@prisma/client";
-import { JWT } from "next-auth/jwt";
+import NextAuth, { DefaultSession } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import prisma from './prisma/client';
+import { compare } from 'bcryptjs';
+import { Customer } from '@prisma/client';
+import { JWT } from 'next-auth/jwt';
 // Your own logic for dealing with plaintext password strings; be careful!
 
-declare module "next-auth" {
+declare module 'next-auth' {
   interface Session {
     user: {
-      id: Customer["customerId"];
-      role: Customer["role"];
-      firstName: Customer["firstName"];
-      lastName: Customer["lastName"];
-    } & DefaultSession["user"];
+      id: Customer['customerId'];
+      role: Customer['role'];
+      firstName: Customer['firstName'];
+      lastName: Customer['lastName'];
+      customerReference: Customer['customerReference'];
+    } & DefaultSession['user'];
   }
 }
 
-declare module "next-auth/jwt" {
+declare module 'next-auth/jwt' {
   /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
   interface JWT {
-    id: Customer["customerId"];
-    role: Customer["role"];
-    firstName: Customer["firstName"];
-    lastName: Customer["lastName"];
+    id: Customer['customerId'];
+    role: Customer['role'];
+    firstName: Customer['firstName'];
+    lastName: Customer['lastName'];
+    customerReference: Customer['customerReference'];
   }
 }
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   session: {
-    strategy: "jwt",
+    strategy: 'jwt',
   },
   pages: {
-    signIn: "/auth/signin",
-    signOut: "/auth/signout",
-    error: "/auth/error",
+    signIn: '/auth/signin',
+    signOut: '/auth/signout',
+    error: '/auth/error',
   },
   providers: [
     Credentials({
@@ -42,13 +44,13 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       // e.g. domain, username, password, 2FA token, etc.
       credentials: {
         email: {
-          label: "Email",
-          type: "email",
-          placeholder: "test@test.com",
+          label: 'Email',
+          type: 'email',
+          placeholder: 'test@test.com',
         },
         password: {
-          label: "Password",
-          type: "password",
+          label: 'Password',
+          type: 'password',
         },
       },
       authorize: async (credentials) => {
@@ -91,13 +93,14 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           lastName: user.lastName,
           email: user.email,
           role: user.role,
+          customerReference: user.customerReference,
         };
       },
     }),
   ],
   callbacks: {
     async redirect({ url, baseUrl }) {
-      return baseUrl + "/shop";
+      return baseUrl + '/shop';
     },
     async session({ session, token }) {
       if (token.sub && session.user) {
@@ -113,6 +116,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 
       if (token.lastName && session.user) {
         session.user.lastName = token.lastName;
+      }
+
+      if (token.customerReference && session.user) {
+        session.user.customerReference = token.customerReference;
       }
 
       return session;
@@ -132,6 +139,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       token.sub = u.customerId;
       token.firstName = u.firstName;
       token.lastName = u.lastName;
+      token.customerReference = u.customerReference;
 
       return token;
     },
