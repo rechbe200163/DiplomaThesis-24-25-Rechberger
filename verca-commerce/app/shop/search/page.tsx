aa0230685126b7e4ed112673aa4f9fad0f1b90d1
Michael Rechberger <rechbe200163@sr.htlweiz.at>
2024-11-15 17:56:08 +0100

    fix: update SearchPage to use productId instead of id in ProductCard for consistency

--- verca-commerce/app/shop/search/page.tsx
+++ verca-commerce/app/shop/search/page.tsx
@@ -17,7 +17,7 @@ export default async function SearchPage(props: {
       {products.length > 0 ? (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 items-center gap-4'>
           {products.map((product) => (
-            <ProductCard key={product.id} product={product} />
+            <ProductCard key={product.productId} product={product} />
           ))}
         </div>
       ) : (
