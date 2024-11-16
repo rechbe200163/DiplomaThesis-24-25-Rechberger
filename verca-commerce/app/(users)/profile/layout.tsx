efcca496b09d09de541cc325bdc5bd67383c58d2
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-11-16 15:45:28 +0100

    refactored profile layout to integrate AppSidebar and SidebarProvider for improved structure

--- verca-commerce/app/(users)/profile/layout.tsx
+++ verca-commerce/app/(users)/profile/layout.tsx
@@ -2,9 +2,14 @@ import { auth } from '@/auth';
 import HeaderBox from '@/components/header/HeaderBox';
 import InteractiviyComponent from '@/components/nav/dashboard/InteractiviyComponent';
 import DashboardFooter from '@/components/nav/dashboard/ProfileIconComponent';
-import SideNav from '@/components/nav/dashboard/SideNav';
+import { AppSidebar } from '@/components/nav/dashboard/SideNav';
 import DashbaordFooterSkeleton from '@/components/skeletons/DashboardFooterSkeleton';
 import SideNavSkeleton from '@/components/skeletons/SideNavSkeleton';
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/sidebar';
 import Link from 'next/link';
 import { Suspense } from 'react';
 
@@ -27,43 +32,14 @@ export default async function Layout({
   }
 
   return (
-    <div className='flex h-screen flex-col md:flex-row md:overflow-hidden bg-gray-100'>
-      {/* Sidebar */}
-      <aside className='flex-none w-full md:w-64 border-r-2 bg-white shadow-md flex flex-col justify-between'>
-        <div>
-          <Suspense fallback={<SideNavSkeleton />}>
-            <SideNav />
-          </Suspense>
-        </div>
-        <div className='p-4'>
-          <Suspense fallback={<DashbaordFooterSkeleton />}>
-            <DashboardFooter />
-          </Suspense>
-        </div>
-      </aside>
-
-      {/* Main content */}
-      <div className='flex-grow flex flex-col'>
-        {/* Header with border */}
-        <div className='p-4 border-b-2 bg-white shadow-sm flex flex-row items-center gap-4 justify-between'>
-          <HeaderBox
-            title='Welcome,'
-            subtext='welcome to your dashboard'
-            name={session.user.lastName}
-            type='greeting'
-          />
-          <InteractiviyComponent />
-        </div>
-
-        {/* Conditional content based on role */}
-        <div className='flex-grow p-4 overflow-auto'>
-          {session.user.role === 'USER' ? (
-            <Suspense fallback={<div>Loading...</div>}>{user}</Suspense>
-          ) : (
-            <Suspense fallback={<div>Loading...</div>}>{admin}</Suspense>
-          )}
-        </div>
-      </div>
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <main className='m-4 space-y-4'>
+          <SidebarTrigger />
+          {session?.user.role === user ? admin : user}
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
