48ffc6758e108850022356c202c95c9a40990e37
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-11-16 15:45:20 +0100

    feat: refactor navigation components to use Sidebar for improved layout and organization

--- verca-commerce/components/nav/dashboard/NavLinksProfile.tsx
+++ verca-commerce/components/nav/dashboard/NavLinksProfile.tsx
@@ -1,41 +1,48 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import clsx from "clsx";
-import { HomeIcon, LucideListOrdered } from "lucide-react";
-import { PiInvoice } from "react-icons/pi";
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+import { HomeIcon, LucideListOrdered } from 'lucide-react';
+import { PiInvoice } from 'react-icons/pi';
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar';
+import { Item } from '@radix-ui/react-select';
+import { cn } from '@/lib/utils';
 
 const links = [
-  { name: "Account", href: "/profile", icon: HomeIcon },
-  { name: "Orders", href: "/profile/orders", icon: LucideListOrdered },
-  { name: "Invoices", href: "/profile/invoices", icon: PiInvoice },
+  { name: 'Account', href: '/profile', icon: HomeIcon },
+  { name: 'Orders', href: '/profile/orders', icon: LucideListOrdered },
+  { name: 'Invoices', href: '/profile/invoices', icon: PiInvoice },
 ];
 
 export default function NavLinksProfile() {
   const pathname = usePathname();
   return (
-    <>
+    <SidebarMenu>
+      {/* <Search placeholder='Search ' /> */}
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <Link
-            key={link.name}
-            href={link.href}
-            className={clsx(
-              "flex items-center gap-2 rounded-md p-3 text-sm font-medium hover:opacity-100",
-              {
-                "bg-blue-600 text-white opacity-100 ": pathname === link.href,
-                "bg-gray-100 hover:bg-blue-100 hover:text-blue-600":
-                  pathname !== link.href,
-              }
-            )}
-          >
-            <LinkIcon className="w-6 h-6" />
-            <p className="hidden md:block">{link.name}</p>
-          </Link>
+          <SidebarMenuItem key={link.href}>
+            <SidebarMenuButton
+              asChild
+              className={cn(
+                'flex items-center justify-start w-full px-4 py-2',
+                pathname === link.href ? 'bg-gray-300' : 'hover:bg-gray-100'
+              )}
+            >
+              <Link href={link.href}>
+                <LinkIcon className='w-6 h-6' />
+                <span>{link.name}</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
         );
       })}
-    </>
+    </SidebarMenu>
   );
 }
--- verca-commerce/components/nav/dashboard/SideNav.tsx
+++ verca-commerce/components/nav/dashboard/SideNav.tsx
@@ -1,16 +1,58 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+} from '@/components/ui/sidebar';
+import Image from 'next/image';
 import NavLinksProfile from './NavLinksProfile';
-import CompanyIconComponent from './CompanyIconComponent';
+import { HomeIcon } from '@radix-ui/react-icons/dist/HomeIcon';
+import { ChevronDown, LucideListOrdered } from 'lucide-react';
+import { PiInvoice } from 'react-icons/pi';
+import Link from 'next/link';
+import {
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '@/components/ui/collapsible';
+import { Collapsible } from '@radix-ui/react-collapsible';
 
-export default function SideNav() {
+export function AppSidebar() {
   return (
-    <section className='flex h-full flex-col px-3 py-4 md:px-2'>
-      <CompanyIconComponent />
-      <div className='pb-3'>
-        {/* <Search placeholder="Search anything" /> */}
-      </div>
-      <div className='flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2'>
-        <NavLinksProfile />
-      </div>
-    </section>
+    <Sidebar collapsible='icon' variant='inset'>
+      <SidebarHeader>
+        <div className='flex items-center justify-center'>
+          <Image
+            src='/assets/images/logo.svg'
+            width={200}
+            height={200}
+            alt='Logo'
+            className='w-8 h-8'
+          />
+          <h1 className='text-lg font-bold ml-2'>Verca</h1>
+        </div>
+      </SidebarHeader>
+      <SidebarContent>
+        <Collapsible defaultOpen className='group/collapsible'>
+          <SidebarGroup>
+            <SidebarGroupLabel asChild>
+              <CollapsibleTrigger>
+                History
+                <ChevronDown className='ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180' />
+              </CollapsibleTrigger>
+            </SidebarGroupLabel>
+            <CollapsibleContent>
+              <SidebarGroupContent>
+                <NavLinksProfile />
+              </SidebarGroupContent>
+            </CollapsibleContent>
+          </SidebarGroup>
+        </Collapsible>
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
   );
 }
