959b6798666307a51cfb2057d3944647c1663d35
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-11-17 16:22:22 +0100

    refactored GET handler to support order details retrieval and improve customerReference handling

--- verca-commerce/app/api/orders/[customerReference]/route.ts
+++ verca-commerce/app/api/orders/[customerReference]/route.ts
@@ -1,22 +1,52 @@
 import prisma from '@/prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
+
 export async function GET(
   req: NextRequest,
   props: { params: Promise<{ customerReference: string }> }
 ) {
   const params = await props.params;
+
   try {
-    const customerReference = params.customerReference;
-    const orders = await prisma.order.findMany({
-      where: {
-        customerReference: customerReference,
-      },
-      include: {
-        customer: true,
-      },
-    });
-    return NextResponse.json(orders, { status: 200 });
+    const customerReference = Number(params.customerReference);
+    const orderId = req.nextUrl.searchParams.get('orderId');
+
+    if (orderId) {
+      // Detailansicht einer Bestellung
+      const orderDetails = await prisma.order.findUnique({
+        where: { orderId, deleted: false },
+        include: {
+          products: {
+            include: {
+              product: true, // Produktdetails
+            },
+          },
+          invoice: true, // Rechnungsdetails
+        },
+      });
+
+      if (!orderDetails) {
+        return NextResponse.json({ error: 'Order not found' }, { status: 404 });
+      }
+
+      return NextResponse.json(orderDetails, { status: 200 });
+    } else {
+      // Übersicht pro Bestellung
+      const orders = await prisma.order.findMany({
+        where: { customerReference, deleted: false },
+        include: {
+          products: {
+            include: {
+              product: true, // Produktdetails
+            },
+          },
+        },
+      });
+
+      return NextResponse.json(orders, { status: 200 });
+    }
   } catch (error) {
+    console.error('Error fetching orders:', error);
     return NextResponse.json(
       { error: 'Internal Server Error' },
       { status: 500 }
