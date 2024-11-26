cc0553ef3637becd34d33df76cfe68c06426e9d3
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-11-26 18:26:17 +0100

    update next.config.mjs to support remote image patterns for enhanced image loading

--- verca-commerce/next.config.mjs
+++ verca-commerce/next.config.mjs
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["img.daisyui.com"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.daisyui.com',
+        port: '',
+        pathname: '/images/**',
+        search: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+    ],
   },
 };
 
