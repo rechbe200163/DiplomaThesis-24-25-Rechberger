96561fe7bbabe146b3f59889c14c029466306a97
Michael Rechberger <rechbe200163@sr.htlweiz.at>
2024-11-26 15:56:15 +0100

    refactored GET handler to retrieve order details based on customerReference and improve error handling

--- verca-commerce/app/api/orders/[customerReference]/route.ts
+++ verca-commerce/app/api/orders/[customerReference]/route.ts
@@ -9,42 +9,25 @@ export async function GET(
 
   try {
     const customerReference = Number(params.customerReference);
-    const orderId = req.nextUrl.searchParams.get('orderId');
 
-    if (orderId) {
-      // Detailansicht einer Bestellung
-      const orderDetails = await prisma.order.findUnique({
-        where: { orderId, deleted: false },
-        include: {
-          products: {
-            include: {
-              product: true, // Produktdetails
-            },
-          },
-          invoice: true, // Rechnungsdetails
-        },
-      });
-
-      if (!orderDetails) {
-        return NextResponse.json({ error: 'Order not found' }, { status: 404 });
-      }
-
-      return NextResponse.json(orderDetails, { status: 200 });
-    } else {
-      // Übersicht pro Bestellung
-      const orders = await prisma.order.findMany({
-        where: { customerReference, deleted: false },
-        include: {
-          products: {
-            include: {
-              product: true, // Produktdetails
-            },
+    // Detailansicht einer Bestellung
+    const orderDetails = await prisma.order.findMany({
+      where: { customerReference, deleted: false },
+      include: {
+        products: {
+          include: {
+            product: true, // Produktdetails
           },
         },
-      });
+        invoice: true, // Rechnungsdetails
+      },
+    });
 
-      return NextResponse.json(orders, { status: 200 });
+    if (!orderDetails) {
+      return NextResponse.json({ error: 'Order not found' }, { status: 404 });
     }
+
+    return NextResponse.json(orderDetails, { status: 200 });
   } catch (error) {
     console.error('Error fetching orders:', error);
     return NextResponse.json(
