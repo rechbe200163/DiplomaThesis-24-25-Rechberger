7dcac81be6194d1981881931afee519679439470
Michael Rechberger <rechbe200163@sr.htlweiz.at>
2024-12-02 10:15:26 +0100

    enhanced padding in OrdersComponent for improved layout

--- verca-commerce/components/cards/dashboard/OrdersComponent.tsx
+++ verca-commerce/components/cards/dashboard/OrdersComponent.tsx
@@ -6,7 +6,7 @@ import OrderCart from './OrderCart';
 
 function Orders({ orders }: { orders: OrderDetails[] }) {
   return (
-    <div className='grid grid-row-1 sm:grid-row-2 lg:grid-row-3 gap-6 p-6'>
+    <div className='grid grid-row-1 sm:grid-row-2 lg:grid-row-3 gap-6 py-6'>
       {orders.map((order) => (
         <OrderCart key={order.orderId} order={order} />
       ))}
