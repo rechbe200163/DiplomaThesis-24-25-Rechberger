1b57b84e55d0c8591a420406a11d8c6b7dffd888
Michael Rechberger <rechbe200163@sr.htlweiz.at>
2024-12-02 09:00:37 +0100

    implemented pagination for user orders: integrated pagination component and updated order retrieval logic

--- verca-commerce/app/(users)/profile/@user/orders/page.tsx
+++ verca-commerce/app/(users)/profile/@user/orders/page.tsx
@@ -1,20 +1,35 @@
 import { auth } from '@/auth';
 import React from 'react';
-import { getOrdersByCustomer } from '@/lib/data.dashboard';
-import OrderCard from '@/components/cards/dashboard/OrdersComponent';
+import {
+  getOrdersByCustomerPagination,
+  getTotalOrders,
+} from '@/lib/data.dashboard';
 import Orders from '@/components/cards/dashboard/OrdersComponent';
 
-async function UserOrdersPage() {
+import PaginationComponent from '@/components/pagination/PaginationComponent';
+
+async function UserOrdersPage(props: {
+  searchParams?: Promise<{
+    page?: string;
+  }>;
+}) {
   const session = await auth();
   if (!session) return null;
   const customerReference = session?.user?.customerReference;
-  const order = await getOrdersByCustomer(customerReference!);
+
+  const searchParams = await props.searchParams;
+  const page = searchParams?.page ? parseInt(searchParams.page) : 1;
+
+  const orders = await getOrdersByCustomerPagination(customerReference!, page);
+  const { totalOrders } = await getTotalOrders(customerReference);
+  const ORDERS_PER_PAGE = 5;
+  const totalPages = Math.ceil(totalOrders / ORDERS_PER_PAGE);
+
   return (
-    <div>
+    <div className=''>
       <h1 className='sticky top-10'>Orders</h1>
-      <ul>
-        <Orders orders={order} />
-      </ul>
+      <Orders orders={orders} />
+      <PaginationComponent totalPages={totalPages} />
     </div>
   );
 }
