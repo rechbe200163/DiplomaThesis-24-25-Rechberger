f6312f0e30ec01c9326453c5167eb9441524eca1
Michael Rechberger <rechbe200163@sr.htlweiz.at>
2024-12-18 14:18:46 +0100

    added custom data-theme attribute to html element for theming support

--- verca-commerce/app/layout.tsx
+++ verca-commerce/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' data-theme={'custom'}>
       <body className={inter.className}>
         <main>{children}</main>
         <Toaster />
