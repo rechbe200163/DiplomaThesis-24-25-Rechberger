48a69e7a147de325706032c5eaca7d17eac296b0
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-12-25 11:12:56 +0100

    enhanced UploadForm and UserDetailsCard for improved user avatar management

--- verca-commerce/components/cards/profile/AccountComponent.tsx
+++ verca-commerce/components/cards/profile/AccountComponent.tsx
@@ -6,17 +6,19 @@ import ImageComponent from '@/components/images/ImageComponent';
 async function UserDetailsCard() {
   const session = await auth();
   const user = await fetchUser(session?.user.customerReference!);
+
   return (
-    <div className='relative w-24 h-24 rounded-full overflow-hidden group'>
+    <div className='relative overflow-hidden group'>
       <ImageComponent
         imagePath={user?.avatarPath!}
         alt='User Avatar'
         width={100}
         height={100}
-        classname='group-hover:opacity-0 object-fill'
+        classname='object-fill'
       />
       <UploadForm />
     </div>
   );
 }
+
 export default UserDetailsCard;
--- verca-commerce/components/forms/dashboard/UploadForm.tsx
+++ verca-commerce/components/forms/dashboard/UploadForm.tsx
@@ -1,8 +1,11 @@
 'use client';
+import { useRef } from 'react';
 import { useToast } from '@/hooks/use-toast';
 import { processImage } from '@/lib/actions/user.actions';
 import { PencilSquareIcon } from '@heroicons/react/24/solid';
-import { useActionState, useRef } from 'react';
+import { useActionState } from 'react';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
 export default function UploadForm() {
   const { toast } = useToast();
@@ -13,34 +16,17 @@ export default function UploadForm() {
     },
   });
 
-  // // show toast on success
-  // if (formState?.success) {
-  //   toast({
-  //     title: 'Image uploaded successfully',
-  //     variant: 'default',
-  //   });
-  // }
-
-  // // show toast on error
-  // if (formState?.errors) {
-  //   toast({
-  //     title: 'Error uploading image' + formState?.errors.title.join(', '),
-  //     variant: 'destructive',
-  //   });
-  // }
-
   return (
-    <form action={action} className='flex flex-col gap-4'>
-      <button
-        type='submit'
-        disabled={isPending}
-        className=' absolute inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 cursor-pointer'
-      >
-        <label>
-          <PencilSquareIcon className='w-6 h-6 text-white' />
-          <input type='file' name='file' />
-        </label>
-      </button>
-    </form>
+    <div>
+      <form action={action}>
+        <Input
+          type='file'
+          name='file'
+          accept='.png, .jpg, .jpeg'
+          className='opacity-0'
+        />
+        <Button>Upload</Button>
+      </form>
+    </div>
   );
 }
