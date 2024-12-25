3a107212a191ec19804390b00b7edabb42c8d75f
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-12-25 10:33:04 +0100

    integrated UploadForm and ImageComponent into UserDetailsCard for enhanced user avatar management

--- verca-commerce/components/cards/profile/AccountComponent.tsx
+++ verca-commerce/components/cards/profile/AccountComponent.tsx
@@ -1,22 +1,22 @@
 import { auth } from '@/auth';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { fetchUser } from '@/lib/data.shop';
-import Image from 'next/image';
+import UploadForm from '@/components/forms/dashboard/UploadForm';
+import ImageComponent from '@/components/images/ImageComponent';
 
 async function UserDetailsCard() {
   const session = await auth();
   const user = await fetchUser(session?.user.customerReference!);
   return (
-    <div className='w-24 h-24 rounded-full overflow-hidden'>
-      <Image
-        src='/pp.png'
-        alt='@shadcn'
+    <div className='relative w-24 h-24 rounded-full overflow-hidden group'>
+      <ImageComponent
+        imagePath={user?.avatarPath!}
+        alt='User Avatar'
         width={100}
         height={100}
-        className='w-full h-full'
+        classname='group-hover:opacity-0 object-fill'
       />
+      <UploadForm />
     </div>
   );
 }
-
 export default UserDetailsCard;
